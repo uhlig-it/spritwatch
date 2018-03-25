70b5df55e166df575fc1c09ed196ebb8766c0586
Steffen Uhlig <steffen@familie-uhlig.net>
2018-03-25 23:20:28 +0200

    Refactor method names

--- lib/sprit_watch/station_registry.rb
+++ lib/sprit_watch/station_registry.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 module SpritWatch
   class StationRegistry
-    attr_reader :stations
-
     def initialize(stations)
       @stations = stations
     end
 
     # TODO: This may fail; we may need to look it up via Client#details or use a NullName (falling back to the station's id)
     def lookup(id)
-      stations.select { |h| h['id'] == id }.first['name']
+      @stations.select { |h| h['id'] == id }.first['name']
     end
 
     def all
-      stations.map { |s| s['id'] }
+      @stations.map { |s| s['id'] }
     end
   end
 end
