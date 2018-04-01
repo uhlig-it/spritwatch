81f0c3473c893961372b2b4b42bf1bdc88b240b0
Steffen Uhlig <steffen@familie-uhlig.net>
2018-04-01 22:54:00 +0200

    Test error conditions for preferences file

--- lib/sprit_watch/preferences.rb
+++ lib/sprit_watch/preferences.rb
@@ -6,20 +6,20 @@ module SpritWatch
   class Preferences
     NoPreferredStations = Class.new(StandardError) do
       def initialize(preferences_file)
-        super("No preferred stations defined in #{preferences_file}")
+        super("No preferred stations defined in #{preferences_file}.")
       end
     end
 
     PreferencesFileNotFound = Class.new(StandardError) do
       def initialize(preferences_file)
-        super("Preferences expected in #{preferences_file} but not found")
+        super("Preferences expected in #{preferences_file} but the file was not found.")
       end
     end
 
-    PREFERENCES_FILE = Pathname('~/.spritwatch.yml').expand_path
+    PREFERENCES_FILE = '~/.spritwatch.yml'
 
     def initialize
-      @preferences_file = PREFERENCES_FILE
+      @preferences_file = Pathname(ENV['SPRITWATCH_PREFERENCES_FILE'] || PREFERENCES_FILE).expand_path
       raise PreferencesFileNotFound, @preferences_file unless @preferences_file.exist?
       @preferences = YAML.load_file(@preferences_file) || {}
     end
--- spec/system/prices_spec.rb
+++ spec/system/prices_spec.rb
@@ -3,9 +3,47 @@
 require 'aruba/rspec'
 
 describe 'prices', type: 'aruba' do
-  it 'warns about missing preferences' do
+  let(:preferences) do
+    {
+      'stations' => [
+        { 'id' => '95d000e0-48a3-41e1-907f-e32dc9d58525', 'name' => 'Aral Königstraße' }
+      ]
+    }
+  end
+
+  before do
+    set_environment_variable('SPRITWATCH_PREFERENCES_FILE', preferences_file)
     run "bundle exec #{aruba.root_directory}/exe/spritwatch prices"
-    expect(last_command_started).to_not be_successfully_executed
-    expect(last_command_started).to have_output(/Error/)
+  end
+
+  context 'with a proper preferences file' do
+    let(:preferences_file) { Tempfile.new.path }
+
+    before do
+      write_file(preferences_file, YAML.dump(preferences))
+    end
+
+    it 'shows the prices of the preferred station' do
+      expect(last_command_started).to be_successfully_executed
+      expect(last_command_started).to have_output(/Aral/)
+    end
+  end
+
+  context 'with an empty preferences file' do
+    let(:preferences_file) { Tempfile.new.path }
+
+    it 'warns about missing preferences' do
+      expect(last_command_started).to_not be_successfully_executed
+      expect(last_command_started).to have_output(/preferred stations defined/)
+    end
+  end
+
+  context 'with a non-existing preferences file' do
+    let(:preferences_file) { Pathname(Dir.tmpdir) / 'does_not_exist.yml' }
+
+    it 'warns about missing preferences' do
+      expect(last_command_started).to_not be_successfully_executed
+      expect(last_command_started).to have_output(/file was not found/)
+    end
   end
 end
