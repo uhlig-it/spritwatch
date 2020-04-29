0363629db25327abf1edfbb7e856a15b7c5dc803
Steffen Uhlig <steffen@familie-uhlig.net>
2020-04-29 19:55:18 +0200

    Update dependencies

--- spec/system/help_spec.rb
+++ spec/system/help_spec.rb
@@ -4,7 +4,7 @@ require 'aruba/rspec'
 
 describe 'help', type: 'aruba' do
   it 'prints usage' do
-    run "bundle exec #{aruba.root_directory}/exe/spritwatch"
+    run_command "bundle exec #{aruba.root_directory}/exe/spritwatch"
     expect(last_command_started).to be_successfully_executed
     expect(last_command_started).to have_output(/Tankerkönig/)
   end
